408119c1041ca3d331ef8486e1652c1cf6459679
Kostya Charkin <93kostya@gmail.com>
2017-11-14 12:32:30 +0200

    add timeout to find_closest function (#21)

--- amorph/utils/search.py
+++ amorph/utils/search.py
@@ -1,21 +1,28 @@
+import time
+
 from ..metrics import string_similarity
 
 
-def find_closest(source, samples: list, metric=string_similarity, key=None):
+def find_closest(source, samples: list, metric=string_similarity, key=None, timeout=None):
     """
     Finds closest code to the source by finding minimum of metric
     :param source: Source code
     :param samples: Iterable of samples to search in
     :param metric: Two string arguments function measuring similarity between two codes
     :param key: Single argument function to get value for metric computing
+    :param timeout: Max time in seconds to find closest code
     :return: Closest to source sample
     """
     max_metric = None
     closest_sample = None
+    start_time = time.time() if timeout is not None else None
     for sample in samples:
         current_metric = metric(source, sample, key)
         if max_metric is None or current_metric > max_metric:
             max_metric = current_metric
             closest_sample = sample
+
+        if timeout is not None and time.time() - start_time >= timeout:
+            break
 
     return closest_sample
--- amorph/utils/test/test_search.py
+++ amorph/utils/test/test_search.py
@@ -1,7 +1,9 @@
 import unittest
 
-from amorph.utils import find_closest
+import time
+
 from amorph.metrics import string_similarity
+from amorph.utils import find_closest
 
 
 class TestSearch(unittest.TestCase):
@@ -11,6 +13,18 @@ class TestSearch(unittest.TestCase):
         sample = find_closest(source, samples, string_similarity)
 
         self.assertEqual(sample, samples[2])
+
+    def test_find_closest_timeout(self):
+        source = 'a + b + c'
+        samples = ['a + b + c + d + e + f'] * 50000
+        samples.append('a + b * c')
+
+        timeout = 0.05
+        start_time = time.time()
+        find_closest(source, samples, string_similarity, timeout=timeout)
+        real_time = time.time() - start_time
+        self.assertAlmostEqual(real_time, timeout, delta=timeout * 0.1)
+
 
 if __name__ == '__main__':
     unittest.main()
